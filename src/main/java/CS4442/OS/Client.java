ff32d4dbdb678ced22a74b0c62468d4ca4a13b46
Adam Byrne <adamrbyrne@gmail.com>
2023-05-09 16:58:16 +0100

    Fixed using else branch that I forgot

--- src/main/java/CS4442/OS/Client.java
+++ src/main/java/CS4442/OS/Client.java
@@ -7,28 +7,17 @@ import java.net.*;
 public class Client {
 	public static void main(String[] args) {
 		try {
-			// Create a client socket and connect to the server on port 1234
 			Socket socket = new Socket("localhost", 1234);
 			System.out.println("Connected to server on port 1234");
 
-			// Create input and output streams for the socket
 			InputStream inputStream = socket.getInputStream();
 			OutputStream outputStream = socket.getOutputStream();
 
-//			// Send a message to the server
-//			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
-//			writer.write("Hello from client\n");
-//			writer.flush();
-//
-//			// Read the response from the server
-//			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-//			String response = reader.readLine();
-//			System.out.println("Received response: " + response);
 			System.out.println("Anything you type will be sent to the server and returned reversed.");
 			System.out.println("Type 'exit' to quit.\n");
+
 			boolean running = true;
 			while (running) {
-				// send text to server form input
 				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 				String message = reader.readLine();
 
@@ -38,17 +27,14 @@ public class Client {
 
 				if (message.equals("exit")) {
 					running = false;
+				}else{
+					reader = new BufferedReader(new InputStreamReader(inputStream));
+					String response = reader.readLine();
+					System.out.println("Received response: " + response);
 				}
-
-				// read response from server
-				reader = new BufferedReader(new InputStreamReader(inputStream));
-				String response = reader.readLine();
-				System.out.println("Received response: " + response);
-
 
 			}
 
-			// Close the socket
 			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/main/java/CS4442/OS/Server.java
+++ src/main/java/CS4442/OS/Server.java
@@ -7,43 +7,32 @@ import java.net.Socket;
 public class Server {
 	public static void main(String[] args) {
 		try {
-			// Create a server socket on port 1234
 			ServerSocket serverSocket = new ServerSocket(1234);
 			System.out.println("Server started on port 1234");
 			Socket clientSocket = serverSocket.accept();
 			System.out.println("Client connected from " + clientSocket.getInetAddress());
 
-			// accept clients continuously
 			boolean running = true;
 			while (running) {
-				// Wait for a client to connect
-
-
-				// Create input and output streams for the client socket
 				InputStream inputStream = clientSocket.getInputStream();
 				OutputStream outputStream = clientSocket.getOutputStream();
 
-				// Read the message from the client
 				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 				String message = reader.readLine();
 				System.out.println("Received message: " + message);
 
-				// if 'exit'
 				if (message.equals("exit")) {
 					running = false;
+				}else {
+					BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+					String reversedMessage = new StringBuilder(message).reverse().toString();
+					writer.write(reversedMessage + "\n");
+					writer.flush();
 				}
-
-				// Send a response to the client
-				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
-				// write reversed message back to client
-				String reversedMessage = new StringBuilder(message).reverse().toString();
-				writer.write(reversedMessage + "\n");
-				writer.flush();
 			}
+
 			clientSocket.close();
-
 			serverSocket.close();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
