e86b0f1164202abb6ef8c28ef50ebb620b8632c4
Adam Byrne <adamrbyrne@gmail.com>
2023-05-11 10:35:57 +0100

    added gititgnore and commands

---
+++ src/Commands.java
@@ -0,0 +1,34 @@
+public class Commands {
+
+	private String[] validCommands = { "/help", "/exit", "/reverse" };
+	
+	public boolean isValidCommand(String command) {
+		for (String validCommand : validCommands) {
+			if (validCommand.equals(command)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String getHelp() {
+		return "Valid commands are: " + String.join(", ", validCommands);
+	}
+	
+	public String reverse(String message) {
+		return new StringBuilder(message).reverse().toString();
+	}
+
+	public String execute(String command, String message) {
+		switch (command) {
+			case "/help":
+				return getHelp();
+			case "/reverse":
+				return reverse(message);
+			case "/exit":
+				return "exit";
+			default:
+				return "Invalid command";
+		}
+	}
+}
