a1d3c87bba16a8fda520482e911667e859263131
Adam Byrne <adamrbyrne@gmail.com>
2023-05-13 20:27:46 +0100

    test commands

--- src/test/java/CS4442/OS/TestCommand.java
+++ src/test/java/CS4442/OS/TestCommand.java
@@ -8,20 +8,41 @@ public class TestCommand {
     private PrintWriter out = new PrintWriter(System.out);
 
     @Test
-    public void helpCommand() {
+    public void helpCommandSignal() {
         Command command = new Command("help");
         assert (command.execute(out) == Command.ServerSignals.HELP);
     }
 
     @Test
-    public void listCommand() {
+    public void listCommandSignal() {
         Command command = new Command("list");
         assert (command.execute(out) == Command.ServerSignals.LIST);
     }
 
     @Test
-    public void quitCommand() {
+    public void quitCommandSignal() {
         Command command = new Command("quit");
         assert (command.execute(out) == Command.ServerSignals.QUIT);
     }
+
+    @Test
+    public void invalidCommand() {
+        try {
+            new Command("");
+            assert (false);
+        } catch (IllegalArgumentException e) {
+            assert (true);
+        }
+    }
+
+    @Test
+    public void nullCommand() {
+        try {
+            new Command(null);
+            assert (false);
+        } catch (IllegalArgumentException e) {
+            assert (true);
+        }
+    }
+
 }
