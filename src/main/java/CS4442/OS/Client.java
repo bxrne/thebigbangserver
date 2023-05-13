c10ea204499392f627f6733557200ae3df2a6b13
Adam Byrne <adamrbyrne@gmail.com>
2023-05-13 13:35:00 +0100

    typo

--- src/main/java/CS4442/OS/Client.java
+++ src/main/java/CS4442/OS/Client.java
@@ -74,8 +74,7 @@ public class Client implements Runnable {
 
 	public static void main(String[] args) {
 		Client client = new Client();
-		Thread clientThread = new Thread(client);
-		clientThread.start();
+		client.run();
 	}
 
 }
